d0c5c07ec5df902268e9265b3d1efb94eae72636
Ming Kit Lee <angus_lee@kamigumi.com.hk>
2021-06-28 17:10:55 +0800

    28-6-2021

--- AirLabel/View/PrintLabelView.xaml.cs
+++ AirLabel/View/PrintLabelView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AirLabel.ViewModel;
 
 namespace AirLabel.View
 {
@@ -22,6 +23,8 @@ namespace AirLabel.View
         public PrintLabelView()
         {
             InitializeComponent();
+            //PrintLabelViewModel vm = new PrintLabelViewModel(this);
+            //this.DataContext = vm;
         }
     }
 }
--- AirLabel/ViewModel/PrintLabelViewModel.cs
+++ AirLabel/ViewModel/PrintLabelViewModel.cs
@@ -3,23 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Reporting.WinForms;
 using Microsoft.Toolkit.Mvvm;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using AirLabel.View;
+
 
 namespace AirLabel.ViewModel
 {
-    class PrintLabelViewModel: ObservableObject
+    class PrintLabelViewModel : ObservableObject
     {
 
-        MainWindowViewModel _parentWindow;
+        public IRelayCommand PreviewCommand { get; }
+        public string AirWayBillNo { get; set; }
+
+        
+        private MainWindowViewModel _parentWindow;
+        private ReportViewer _reportviewer;
+
+        //private ReportViewer _reportviewer;
 
         public PrintLabelViewModel(MainWindowViewModel parentWindow)
         {
             _parentWindow = parentWindow;
-           
+            //this._reportviewer = window._reportviewer;
+            PreviewCommand = new RelayCommand(PreviewLabel);
         }
 
+        private void PreviewLabel()
+        {
 
+            //_reportviewer.LocalReport.DataSources.Clear();
+
+            //var rpds_model1 = new ReportDataSource() { Name = "dsShopReport", Value = dataShopReport };
+
+            //_reportviewer.LocalReport.DataSources.Add(rpds_model1);
+
+            ReportParameter[] parameters = new ReportParameter[1];
+            parameters[0] = new ReportParameter("pAirWayBillNo", AirWayBillNo);
+
+            _reportviewer.LocalReport.EnableExternalImages = true;
+
+            //string _path = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+            //string ContentStart = _path + @"\MVVMNvocc\Reports\DeliveryOrder.rdlc";
+
+            string ReportName = "AirLabel";
+            //string ReportName = "DeliveryReceipt";
+            _reportviewer.LocalReport.ReportPath = string.Format("Report/{0}.rdlc", ReportName);
+
+
+            //_reportviewer.LocalReport.ReportPath = ContentStart;
+
+            //string ReportName = "ShippingOrder";
+            //_reportviewer.LocalReport.ReportPath = string.Format("Reports/{0}.rdlc", ReportName); ;
+
+            _reportviewer.SetDisplayMode(DisplayMode.PrintLayout);
+            _reportviewer.LocalReport.SetParameters(parameters);
+            _reportviewer.Refresh();
+            _reportviewer.RefreshReport();
+        }
     }
+
 }
+    
+
